48a920a9cba3ab6510df9efa0e8afd1b00d2e75b
Paula Jimenez <pjimenezm@viewnext.com>
2021-02-03 13:20:31 +0100

    Añadido Robotest

--- SearchdevopsTest.java
+++ SearchdevopsTest.java
@@ -38,23 +38,13 @@ public class SearchdevopsTest {
   }
   @Test
   public void searchdevops() {
-    // Test name: search-devops
-    // Step # | name | target | value
-    // 1 | open | / | 
     driver.get("https://www.google.com/");
-    // 2 | selectFrame | index=0 | 
     driver.switchTo().frame(0);
-    // 3 | click | css=#introAgreeButton .RveJvd | 
     driver.findElement(By.cssSelector("#introAgreeButton .RveJvd")).click();
-    // 4 | selectFrame | relative=parent | 
     driver.switchTo().defaultContent();
-    // 5 | click | name=q | 
     driver.findElement(By.name("q")).click();
-    // 6 | type | name=q | devops
     driver.findElement(By.name("q")).sendKeys("devops");
-    // 7 | sendKeys | name=q | ${KEY_ENTER}
     driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
-    // 8 | click | css=.g:nth-child(10) .LC20lb > span | 
-    driver.findElement(By.cssSelector(".g:nth-child(10) .LC20lb > span")).click();
+    driver.findElement(By.xpath("//div[@id=\'rso\']/div[3]/div/div/a/h3/span")).click();
   }
 }
