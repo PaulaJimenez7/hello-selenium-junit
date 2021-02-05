a50140b96749f31065dc8eb259fe905ec93dd400
Administrator <admin@example.com>
2021-02-05 12:07:26 +0000

    Update SearchRemoteTest.java

--- src/test/java/com/example/hello_selenium_junit/SearchRemoteTest.java
+++ src/test/java/com/example/hello_selenium_junit/SearchRemoteTest.java
@@ -63,7 +63,7 @@ public class SearchRemoteTest {
         driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
         // 8 | click | css=.g:nth-child(10) .LC20lb > span |
         //driver.findElement(By.xpath("//div[@id=\'rso\']/div[3]/div/div/a/h3/span")).click();
-        WebElement googleresult = new WebDriverWait(driver,7)
+        WebElement googleresult = new WebDriverWait(driver,20)
                 .until(driver -> driver.findElement(By.xpath("//div[@id=\'rso\']/div[3]/div/div/a/h3/span")));
         driver.findElement(By.xpath("//div[@id=\'rso\']/div[3]/div/div/a/h3/span")).click();
     }
